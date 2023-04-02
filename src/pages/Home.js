432dded0c2e85dab7509b142005d4a4c7c677647
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2023-04-02 17:03:25 +0200

    Update `Home.js`

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
 
   return (
     <>
-      <Nav />
+      <Nav minimal={false} />
       <div className='home'>
         <h1>Swipe Right®</h1>
         <button className='primary-button' onClick={handleClick}>
