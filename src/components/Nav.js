fbbc09ca4f6f3c08d34a51d2f0afd420099e29b1
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2023-04-02 17:05:14 +0200

    Import logos and add nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,16 @@
-const Nav = () => {
-  return <div>Nav</div>
+import whiteLogo from '../images/tinder_logo_white.png'
+import colorLogo from '../images/color-logo-tinder.png'
+
+// const minimal = true
+
+const Nav = ({ minimal }) => {
+  return (
+    <nav>
+      <div className='logo-container'>
+        <img className='logo' src={minimal ? colorLogo : whiteLogo} />
+      </div>
+    </nav>
+  )
 }
 
 export default Nav
