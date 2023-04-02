35b2bdd61313e31c03cc5b8b71e23d119c61ca5b
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2023-04-02 10:34:14 +0200

    Create h1, signout and create account button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
 const Home = () => {
-  return <div>Home</div>
+
+const authToken = true
+
+const handleClick = () => {
+  console.log("clicked")
+}
+
+  return (
+    <div className='home'>
+      <h1>Swipe Right®</h1>
+      <button className="primary-button" onClick={handleClick}>
+        { authToken : ? "Signout" : "Create Account"}
+      </button>
+    </div>
+  )
 }
 
 export default Home
+ 
