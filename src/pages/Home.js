171c8929a725d3a31dbf25ca13e46581b23bdf5f
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2023-04-02 10:52:23 +0200

    Update `Home.js`

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,23 @@
+import Nav from '../components/Nav'
+
 const Home = () => {
+  const authToken = true
 
-const authToken = true
-
-const handleClick = () => {
-  console.log("clicked")
-}
+  const handleClick = () => {
+    console.log('clicked')
+  }
 
   return (
-    <div className='home'>
-      <h1>Swipe Right®</h1>
-      <button className="primary-button" onClick={handleClick}>
-        { authToken : ? "Signout" : "Create Account"}
-      </button>
-    </div>
+    <>
+      <Nav />
+      <div className='home'>
+        <h1>Swipe Right®</h1>
+        <button className='primary-button' onClick={handleClick}>
+          {authToken ? 'Signout' : 'Create Account'}
+        </button>
+      </div>
+    </>
   )
 }
 
 export default Home
- 
