7b3f90923049f420647d7ff20d25a33ad9a8989d
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2023-04-02 20:11:46 +0200

    Update `Nav.js`

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,14 @@ import colorLogo from '../images/color-logo-tinder.png'
 
 // const minimal = true
 
-const Nav = ({ minimal }) => {
+const Nav = ({ minimal, authToken }) => {
   return (
     <nav>
       <div className='logo-container'>
         <img className='logo' src={minimal ? colorLogo : whiteLogo} />
       </div>
+
+      {!authToken && <button className='nav-button'>Log in</button>}
     </nav>
   )
 }
