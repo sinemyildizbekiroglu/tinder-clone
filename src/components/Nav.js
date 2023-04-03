ebb9a57e3a225d26fed82003d66febfd106d069e
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2023-04-03 17:16:13 +0200

    Add conditional rendering to display login button only when authToken is not present and minimal flag is false

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ const Nav = ({ minimal, authToken }) => {
         <img className='logo' src={minimal ? colorLogo : whiteLogo} />
       </div>
 
-      {!authToken && <button className='nav-button'>Log in</button>}
+      {!authToken && !minimal && <button className='nav-button'>Log in</button>}
     </nav>
   )
 }
